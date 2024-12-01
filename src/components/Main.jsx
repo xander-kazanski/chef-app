1fe92472a16e335c249eb41e35a84e493d629306
Alexander Kazanski <alexanderkazanski@outlook.com>
2024-12-01 14:04:47 -0500

    add styles and push ingredient to state

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./main.css"
 
 function Main(props) {
+  const [ingredients, setIngredients] = useState(["Chicken", "Oregano", "Tomatoes"])
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    setIngredients(prevIngredients => [...prevIngredients, formData.get("ingredient")])
+  }
+
+  const ingredientsListItems = ingredients.map(ingredient => <li key={ingredient}>{ingredient}</li>)
+
   return (
     <main>
-      <form className="add-ingredient-form">
-        <input 
-          aria-label="Add ingredient" 
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
+        <input
+          aria-label="Add ingredient"
           placeholder="e.g. oregano"
           type="text"
+          name="ingredient"
         />
         <button>+ Add ingredient</button>
       </form>
+      <ul>
+        {ingredientsListItems}
+      </ul>
     </main>
   )
 }
